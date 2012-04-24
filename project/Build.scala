817650e5fc3562360d02b61795f9d70413faa376
gakuzzzz <gaku_github@t2v.jp>
2012-04-24 22:47:30 +0900

    cleanup

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 import PlayProject._
 
 object ApplicationBuild extends Build {
-
+	
   val appVersion = "0.1"
 
   lazy val module = Project("play20-auth-module", file("module"))
@@ -13,7 +13,6 @@ object ApplicationBuild extends Build {
   )
 
   lazy val root = PlayProject("sample", appVersion, appDependencies, mainLang = SCALA).settings(
-//	    scalaVersion := "2.9.1"
   ).dependsOn(module)
 
 }
