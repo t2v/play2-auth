ce3c1f4903763e60be9996d44045bdd62d256a5c
Mike Bryant <mikesname@gmail.com>
2015-06-09 16:31:16 +0100

    Prevent assertion error when using the `withLoggedIn` test helper.
    Access the cookie params via the protected members of `CookieTokenAccessor`,
    rather than via the (deprecated) members of `AuthConfig`, which now throw
    an error.
    
    I trust it's not this change which is causing the cache-related test failures!

--- test/src/main/scala/jp/t2v/lab/play2/auth/test/Helpers.scala
+++ test/src/main/scala/jp/t2v/lab/play2/auth/test/Helpers.scala
@@ -2,7 +2,7 @@ package jp.t2v.lab.play2.auth.test
 
 import play.api.test._
 import play.api.mvc.Cookie
-import jp.t2v.lab.play2.auth.AuthConfig
+import jp.t2v.lab.play2.auth.{AuthConfig, CookieTokenAccessor}
 import play.api.libs.Crypto
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -10,12 +10,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 trait Helpers {
 
-  implicit class AuthFakeRequest[A](fakeRequest: FakeRequest[A]) {
+  implicit class AuthFakeRequest[A](fakeRequest: FakeRequest[A]) extends CookieTokenAccessor {
 
     def withLoggedIn(implicit config: AuthConfig): config.Id => FakeRequest[A] = { id =>
       val token = Await.result(config.idContainer.startNewSession(id, config.sessionTimeoutInSeconds)(fakeRequest, global), 10.seconds)
       val value = Crypto.sign(token) + token
-      import config._
       fakeRequest.withCookies(Cookie(cookieName, value, None, cookiePathOption, cookieDomainOption, cookieSecureOption, cookieHttpOnlyOption))
     }
 
